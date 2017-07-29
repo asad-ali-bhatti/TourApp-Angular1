b5d8c126b07a68afcaba054f999698216289331a
Asad Ali Bhatti <asadalibhatti@hotmail.com>
2017-07-30 02:05:38 +0500

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tourAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the tourAppApp
+ */
+angular.module('tourAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name tourAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the tourAppApp
+ */
+angular.module('tourAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
